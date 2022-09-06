c74d3e7596b7655007a0b5ed2c55bd7963ea8cb9
zihean <zihe.an@foxmail.com>
2022-09-06 11:12:58 +0800

    创建Xcode工程

---
+++ exercise/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  exercise
+//
+//  Created by 安子和 on 2022/9/6.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
